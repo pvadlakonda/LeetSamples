aefaf4cc287ddb880e69bd65b63b3e68992ec2f1
Praveen Vadlakonda <Praveen.Vadlakonda@aa.com>
2019-09-24 14:48:44 -0500

    Add Two Numbers Tests

--- src/test/java/com/leetcode/samples/AddTwoNumbersTest.java
+++ src/test/java/com/leetcode/samples/AddTwoNumbersTest.java
@@ -10,17 +10,59 @@ class AddTwoNumbersTest {
     @Test
     void shouldReturnListWithAddedNumbersInReverse() {
 
-        ListNode listNode1c = new ListNode(3);
-        ListNode listNode1b = new ListNode(4, listNode1c);
-        ListNode listNode1a = new ListNode(2, listNode1b);
+        ListNode input1 = convertToListNode(342);
+        ListNode input2 = convertToListNode(465);
 
-        ListNode listNode2c = new ListNode(4);
-        ListNode listNode2b = new ListNode(6, listNode2c);
-        ListNode listNode2a = new ListNode(5, listNode2b);
+        ListNode listNodeResult = addTwoNumbers.addTwoNumbers(input1, input2);
+        assertEquals(708, extractNumberFromList(listNodeResult));
+    }
 
-       ListNode listNodeResult = addTwoNumbers.addTwoNumbers(listNode1a, listNode2a);
+    @Test
+    void test2() {
+        //4003+4445 = 8448
 
+        ListNode input1 = convertToListNode(3004);
+        ListNode input2 = convertToListNode(5444);
 
+        ListNode listNodeResult = addTwoNumbers.addTwoNumbers(input1, input2);
+        assertEquals(8448, extractNumberFromList(listNodeResult));
+    }
 
+    @Test
+    void test3() {
+        //8803+6623 = 15426
+
+        ListNode input1 = convertToListNode(3088);
+        ListNode input2 = convertToListNode(3266);
+
+        ListNode listNodeResult = addTwoNumbers.addTwoNumbers(input1, input2);
+        assertEquals(62451, extractNumberFromList(listNodeResult));
+    }
+
+    private ListNode convertToListNode(int input) {
+        StringBuilder numberString = new StringBuilder(String.valueOf(input));
+        ListNode resultList = null;
+
+        String[] tokens = numberString.toString().split("");
+        int stringLength = tokens.length;
+        ListNode lastNode = new ListNode(Integer.valueOf(tokens[stringLength - 1]));
+        stringLength--;
+        while (stringLength > 0) {
+            ListNode tmpNode = new ListNode(Integer.valueOf(tokens[stringLength - 1]), lastNode);
+            lastNode = tmpNode;
+            resultList = tmpNode;
+            stringLength--;
+        }
+        return resultList;
+
+    }
+
+    private int extractNumberFromList(ListNode listNode) {
+        StringBuilder numberString = new StringBuilder("" + listNode.val);
+        while (listNode.next != null) {
+            numberString.append(listNode.next.val);
+            listNode = listNode.next;
+        }
+        return Integer.parseInt(numberString.toString());
     }
 }
