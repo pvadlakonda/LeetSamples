a5b34ffc3cd1a1f84c29cecdbbed998fb21a0ae1
Praveen Vadlakonda <Praveen.Vadlakonda@aa.com>
2019-10-14 23:36:54 -0500

    string comression

---
+++ src/main/java/com/interview/code/cracking/StringCompression.java
@@ -0,0 +1,33 @@
+package com.interview.code.cracking;
+
+public class StringCompression {
+
+    public String compressedString(String str) {
+        if (str == null || str.length() == 0) {
+            return "";
+        }
+
+        StringBuilder sb = new StringBuilder();
+        char prevChar = 0;
+        int charCount = 1;
+
+        for (char currentChar : str.toCharArray()) {
+            if (currentChar == prevChar) {
+                charCount++;
+            } else {
+                if (charCount > 1) {
+                    sb.append(charCount);
+                    charCount = 1;
+                }
+                sb.append(currentChar);
+            }
+            prevChar = currentChar;
+        }
+
+        if (charCount > 1) {
+            sb.append(charCount);
+        }
+
+        return sb.toString();
+    }
+}
---
+++ src/test/java/com/interview/code/cracking/StringCompressionTest.java
@@ -0,0 +1,22 @@
+package com.interview.code.cracking;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class StringCompressionTest {
+
+    private StringCompression solution = new StringCompression();
+
+    @Test
+    void shouldReturna2b2c2a3Foraabbccaaa() {
+
+        assertEquals("a2b2c2a3", solution.compressedString("aabbccaaa"));
+    }
+
+    @Test
+    void shouldReturnabcForabc() {
+
+        assertEquals("abc", solution.compressedString("abc"));
+    }
+}
