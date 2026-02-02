6764976d11587c066d05ed49953da0bc557a4caa
pvadlakonda <getpraveen@gmail.com>
2026-02-01 20:42:48 -0600

    complete DecodeString implementation

--- src/main/java/com/leetcode/samples/DecodeString.java
+++ src/main/java/com/leetcode/samples/DecodeString.java
@@ -19,20 +19,33 @@ import java.util.Stack;
  */
 public class DecodeString {
 
-    public String decodeString(String s) {// 3[a]2[bc]  3[a2[c]]
-
-        Stack<Integer> stack = new Stack<>();
-        StringBuilder result = new StringBuilder();
-
-        char[] chars = s.toCharArray();
-        int i = 0;
-        while (i < chars.length) {
-            if (chars[i] == '[') {
-                stack.push(i);
+    public String decodeString(String s) {
+        Stack<Integer> countStack = new Stack<>();
+        Stack<StringBuilder> stringStack = new Stack<>();
+        StringBuilder currentString = new StringBuilder();
+        int k = 0;
+        
+        for (char c : s.toCharArray()) {
+            if (Character.isDigit(c)) {
+                k = k * 10 + (c - '0');
+            } else if (c == '[') {
+                countStack.push(k);
+                stringStack.push(currentString);
+                currentString = new StringBuilder();
+                k = 0;
+            } else if (c == ']') {
+                StringBuilder decodedString = stringStack.pop();
+                int currentK = countStack.pop();
+                for (int i = 0; i < currentK; i++) {
+                    decodedString.append(currentString);
+                }
+                currentString = decodedString;
+            } else {
+                currentString.append(c);
             }
         }
-
-        return result.toString();
+        
+        return currentString.toString();
     }
 
 }
--- src/test/java/com/leetcode/samples/DecodeStringTest.java
+++ src/test/java/com/leetcode/samples/DecodeStringTest.java
@@ -6,15 +6,26 @@ import org.junit.jupiter.api.Test;
 
 class DecodeStringTest {
 
-    /*private final DecodeString solution = new DecodeString();
+    private final DecodeString solution = new DecodeString();
 
     @Test
-    void name() {
-
+    void testBasicDecoding() {
         String input = "3[a]2[bc]";
         String expected = "aaabcbc";
-
         assertEquals(expected, solution.decodeString(input));
+    }
 
-    }*/
+    @Test
+    void testNestedDecoding() {
+        String input = "3[a2[c]]";
+        String expected = "accaccacc";
+        assertEquals(expected, solution.decodeString(input));
+    }
+
+    @Test
+    void testMixedDecoding() {
+        String input = "2[abc]3[cd]ef";
+        String expected = "abcabccdcdcdef";
+        assertEquals(expected, solution.decodeString(input));
+    }
 }
